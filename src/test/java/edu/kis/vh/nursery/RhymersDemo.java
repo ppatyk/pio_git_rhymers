5da2b02ad054c5a8acb26e07961e006869bb7d36
pawel <pawelpatyk@interia.com>
2022-02-04 12:46:02 +0100

    1.5.1 RhymersDemo refactoring

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -13,7 +13,10 @@ class RhymersDemo {
 
 	public static void main(String[] args) {
 		RhymersFactory factory = new DefaultRhymersFactory();
+		testRhymers(factory);
+	}
 
+	private static void testRhymers(RhymersFactory factory) {
 		DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
 				factory.getFIFORhymer(), factory.getHanoiRhymer() };
 
@@ -32,7 +35,6 @@ class RhymersDemo {
 		}
 
 		System.out.println("total rejected is " + ((HanoiRhymer) rhymers[RHYMERS_COUNT]).reportRejected());
-
 	}
 
 }
