c07826ea8f18b0fa0d89ec3a74bc38c8356e7b0e
pawel <pawelpatyk@interia.com>
2022-02-04 13:01:02 +0100

    1.5.6 DefaultCountingOutRhymer class documentation

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,11 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Default counting out Rhymer class for integer numbers.
+ * 
+ * @author PPatyk
+ *
+ */
 public class DefaultCountingOutRhymer {
 
 	private static final int TABLE_SIZE = 12;
@@ -9,29 +15,53 @@ public class DefaultCountingOutRhymer {
 
 	private int total = EMPTY_ID;
 
+	/**
+	 * Puts new number to the Rhymer.
+	 * @param in - number to add to the Rhymer.
+	 */
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
 	}
 
+	/**
+	 * Checks if Rhymer is empty.
+	 * @return boolean true if Rhymer is empty.
+	 */
 	public boolean callCheck() {
 		return total == EMPTY_ID;
 	}
 
+	/**
+	 * Gets total numbers in Rhymer.
+	 * @return total numbers in Rhymer.
+	 */
 	public int getTotal() {
 		return total;
 	}
 
+	/**
+	 * Checks if Rhymer is full.
+	 * @return boolean true if Rhymer is full.
+	 */
 	public boolean isFull() {
 		return total == TABLE_SIZE - 1;
 	}
 
+	/**
+	 * Gets last number.
+	 * @return last number value.
+	 */
 	protected int getValue() {
 		if (callCheck())
 			return EMPTY_ID;
 		return numbers[total];
 	}
 
+	/**
+	 * Gets last number from Rhymer and remove it.
+	 * @return value of last number in Rhymer.
+	 */
 	public int countOut() {
 		if (callCheck())
 			return EMPTY_ID;
